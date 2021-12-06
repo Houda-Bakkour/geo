fff75ede31bacc100ed9ae0fe22d50836628d839
houda bakkour <houdabakkour5@gmail.com>
2021-12-06 21:42:51 +0100

    Ajoutez des fichiers projet.

---
+++ geo/Program.cs
@@ -0,0 +1,209 @@
+﻿
+ using System;
+
+using System.Collections.Generic;
+using System.Text;
+namespace geo
+{
+    class Point
+    {
+        public float x, y, z;
+        public void setx(float x)
+        {
+            this.y = x;
+        }
+        public float getx()
+        {
+            return y;
+        }
+        public void sety(float y)
+        {
+            this.y = y;
+        }
+        public float gety()
+        {
+            return y;
+        }
+
+
+        public void setz(float z)
+        {
+            this.z = y;
+        }
+        public float getz()
+        {
+            return z;
+        }
+        public Point(float a, float b, float c)
+        {
+            this.y = a;
+            this.y = b;
+            this.z = c;
+        }
+
+        public Point()
+        {
+        }
+
+        static Double Distance(Point P1, Point P2)
+        {
+            double dx = P1.y - P2.y;
+            double dy = P1.y - P2.y;
+            double dz = P1.z - P2.z;
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+
+        static Boolean egalité(Point p1, Point p2)
+        {
+            if ((p1.y == p2.y) && (p1.y == p2.y) && (p1.z == p2.z))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+
+    }
+
+
+
+
+
+    class Droit
+    {
+        Point p1;
+
+        Point p2;
+        public Droit(Point A, Point B)
+        {
+            this.p1 = A;
+            this.p2 = B;
+        }
+
+        static Double longueurBA(Point A, Point B)
+        {
+            double dx = A.x - B.x;
+            double dy = A.y - B.y;
+            double dz = A.z - B.z;
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        static Boolean paralilisme(Droit AB, Droit AC)
+        {
+            if (AB.p1 != AC.p2)
+            {
+                return true;
+            }
+            return false;
+        }
+        static Boolean intersection(Droit AB, Droit AC)
+        {
+            if (AB.p1 == AC.p2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+
+
+
+    class Circle
+    {
+
+        Point centre;
+        double rayon;
+        public Circle(Point c, double rayon)
+        {
+            this.centre = c;
+            this.rayon = rayon;
+        }
+        public Circle()
+        {
+            centre = new Point();
+
+        }
+
+
+        static void égalité()
+        {
+
+        }
+        static void intersection(Circle C, Circle E)
+        {
+
+        }
+        static void appartenance(Circle A, Circle B)
+        {
+
+        }
+        static double circonférance(double r)
+        {
+            return 2 * 3.14 * r;
+        }
+        static double surface(double r)
+        {
+            return 3.14 * Math.Pow(r, 2);
+        }
+
+    }
+
+
+
+
+
+    class ellipse
+    {
+        float AB, AC;
+        public ellipse(float c, float b)
+        {
+            this.AC = c;
+            this.AB = b;
+        }
+
+        static void paralilisme()
+        {
+
+        }
+        static void intersextion()
+        {
+
+        }
+        static void appartenonce()
+        {
+
+        }
+        static float circonférance(float pAx, float gAx)
+        {
+            float resultat = 0;
+            float pcarre = pAx * pAx;
+            float gcarre = gAx * gAx;
+            float sinus, cosinus;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                sinus = (float)Math.Sin(i * Math.PI / 2000);
+                cosinus = (float)Math.Cos(i * Math.PI / 2000);
+                resultat = (float)(resultat + Math.Sqrt(pcarre * sinus * sinus + gcarre * cosinus * cosinus));
+            }
+            resultat = (float)(resultat * (4 * Math.PI / 2000));
+
+            return resultat;
+        }
+        static float surface(float pAx, float gAx)
+        {
+            return ((float)Math.PI * pAx * gAx);
+
+        }
+
+
+    }
+}
